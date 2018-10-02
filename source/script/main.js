641b212e3cf5532c09ee39695f68ab2101bfc91b
Fennec <shellfix@gmail.com>
2018-10-02 12:33:21 +0300

    скрипт

--- source/script/main.js
+++ source/script/main.js
@@ -16,23 +16,21 @@
 
   // установки по умолчанию
   var slides = document.querySelectorAll(".review__block");
-  var currentWidth = window.innerWidth;
+  var currentWidth = document.body.clientWidth;
   var currentSlide = 0;
   $('.review__button--left').prop('disabled',true);
 
   // реагирование слайдера на ресайз окна
   window.addEventListener("resize", function () {
-    currentWidth = window.innerWidth;
+    currentWidth = document.body.clientWidth;
 
     // если десктоп, то сбрасываем слайдер и показываем по 3
-    if (window.innerWidth > 1200) {
+    if (document.body.clientWidth > 1200) {
       setTimeout( function (){
         $(slides).hide();
         currentSlide = 0;
         $('.review__button--right').prop('disabled',false);
-        $(slides[currentSlide]).show();
-        $(slides[currentSlide]).next().show();
-        $(slides[currentSlide]).next().next().show();
+        $(slides[currentSlide]).show().next().show().next().show();
       }, 200)
     }
     // если меньше десктопа, то сбрасываем слайдер и показываем по 1
@@ -62,18 +60,14 @@
       }
       // поведение шире десктопа
       else {
-        $(slides[currentSlide]).hide();
-        $(slides[currentSlide]).next().hide();
-        $(slides[currentSlide]).next().next().hide();
+        $(slides[currentSlide]).hide().next().hide().next().hide();
         currentSlide ++;
           if (currentSlide > slides.length-4) {
           $('.review__button--right').prop('disabled',true);
         } else {
           $('.review__button--right').prop('disabled',false);
         };
-        $(slides[currentSlide]).fadeIn();
-        $(slides[currentSlide]).next().fadeIn();
-        $(slides[currentSlide]).next().next().fadeIn();
+        $(slides[currentSlide]).fadeIn().next().fadeIn().next().fadeIn()
       }
     });
     //листание влево
@@ -92,18 +86,14 @@
         }
         // поведение при ширине больше десктопа
       else {
-        $(slides[currentSlide]).hide();
-        $(slides[currentSlide]).next().hide();
-        $(slides[currentSlide]).next().next().hide();
+        $(slides[currentSlide]).hide().next().hide().next().hide();
           currentSlide --;
         if (currentSlide < 1) {
           $('.review__button--left').prop('disabled',true);
         } else {
           $('.review__button--left').prop('disabled',false);
         };
-        $(slides[currentSlide]).fadeIn();
-        $(slides[currentSlide]).next().fadeIn();
-        $(slides[currentSlide]).next().next().fadeIn();
+        $(slides[currentSlide]).fadeIn().next().fadeIn().next().fadeIn();
       }
     });
 
@@ -115,8 +105,8 @@
       $(svg[i]).css("fill", "#0047FF");
     });
 
-    var button=document.querySelectorAll(".callback");
-    var modal=document.querySelector(".feadback");
+    var button=document.querySelector(".menu-list-button-open");
+    var modal=document.querySelector(".menu-list-button");
     var closeButton=modal.querySelector(".feadback__close-button");
 
     var isValid = function(sample) {
@@ -134,40 +124,96 @@
       element.classList.add("visually-hidden");
     }
 
-    for (var i = 0; i <= (button.length-1); i++) {
-      button[i].addEventListener ("click", function (evt) {
-        show(evt, modal);
-      });
-    }
+    button.addEventListener ("click", function (evt) {
+      menu.classList.toggle("nav-container--close");
+      over.forEach(function (item) {item.classList.toggle("overlay--close");});
+      burger.classList.toggle("menu-button--close");
+      show(evt, modal);
+    });
 
     closeButton.addEventListener ("click", function (evt) {
       hidden(evt, modal);
     });
 
-    var burger = document.querySelector(".menu-button");
-      var menu = document.querySelector(".nav-container");
-      var over = Array.prototype.slice.call(document.querySelectorAll(".overlay"));
-      var menuItems = Array.prototype.slice.call(menu.querySelectorAll(".menu__item-href"));
+    var button2=document.querySelector(".header-button-open");
+    var modal2=document.querySelector(".header-button");
+    var closeButton2=modal2.querySelector(".feadback__close-button");
+
+    var isValid = function(sample) {
+      var regexp = /\+\d{1}\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}/;
+      return regexp.test(sample);
+    };
+
+    var show = function(e,element) {
+      e.preventDefault();
+      element.classList.remove("visually-hidden");
+    }
+
+    var hidden = function(e,element) {
+      e.preventDefault();
+      element.classList.add("visually-hidden");
+    }
+
+    button2.addEventListener ("click", function (evt) {
+      show(evt, modal2);
+    });
+
+    closeButton2.addEventListener ("click", function (evt) {
+      hidden(evt, modal2);
+    });
+    var button3=document.querySelector(".work-button-open");
+    var modal3=document.querySelector(".work-button");
+    var closeButton3=modal3.querySelector(".feadback__close-button");
+
+    var isValid = function(sample) {
+      var regexp = /\+\d{1}\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}/;
+      return regexp.test(sample);
+    };
+
+    var show = function(e,element) {
+      e.preventDefault();
+      element.classList.remove("visually-hidden");
+    }
+
+    var hidden = function(e,element) {
+      e.preventDefault();
+      element.classList.add("visually-hidden");
+    }
+
+    button3.addEventListener ("click", function (evt) {
+      show(evt, modal3);
+    });
+
+    closeButton3.addEventListener ("click", function (evt) {
+      hidden(evt, modal3);
+    });
 
 
-      menu.classList.add("nav-container--close");
-       burger.classList.add("menu-button--close");
 
-       over.forEach(function (item) {item.classList.add("overlay--close");});
+ var burger = document.querySelector(".menu-button");
+   var menu = document.querySelector(".nav-container");
+   var over = Array.prototype.slice.call(document.querySelectorAll(".overlay"));
+   var menuItems = Array.prototype.slice.call(menu.querySelectorAll(".menu__item-href"));
 
-        burger.addEventListener ("click", function (evt) {
-          evt.preventDefault();
-          menu.classList.toggle("nav-container--close");
-          over.forEach(function (item) {item.classList.toggle("overlay--close");});
-          burger.classList.toggle("menu-button--close");
-       });
-       menuItems.forEach(function (item) {
-         item.addEventListener ("click", function (evt) {
-           menu.classList.toggle("nav-container--close");
-           over.forEach(function (item) {item.classList.toggle("overlay--close");});
-           burger.classList.toggle("menu-button--close");
-         });
-       });
+
+   menu.classList.add("nav-container--close");
+    burger.classList.add("menu-button--close");
+
+    over.forEach(function (item) {item.classList.add("overlay--close");});
+
+     burger.addEventListener ("click", function (evt) {
+       evt.preventDefault();
+       menu.classList.toggle("nav-container--close");
+       over.forEach(function (item) {item.classList.toggle("overlay--close");});
+       burger.classList.toggle("menu-button--close");
+    });
+    menuItems.forEach(function (item) {
+      item.addEventListener ("click", function (evt) {
+        menu.classList.toggle("nav-container--close");
+        over.forEach(function (item) {item.classList.toggle("overlay--close");});
+        burger.classList.toggle("menu-button--close");
+      });
+    });
 
     var examplesButton = document.querySelector(".examples__button");
     var examplesBox = document.querySelector(".examples__box--2");
@@ -177,6 +223,7 @@
       examplesBox.classList.toggle("open");
       examplesButton.classList.toggle("open-btn");
     });
+
 
     var imagesArray = [
       "./images/work-desktop.png",
@@ -191,133 +238,61 @@
           "./images/work-mobile-offroad.png",
           "./images/work-mobile-prem.png"];
           var pricesArray = [
-            '40 800', '37 600', '48 800'
+            '40 800', '40 800', '37 600'
           ];
 
-        var tabs = document.querySelector('.tabs__item');
-    var workButtons = Array.prototype.slice.call(tabs.querySelectorAll(".work__options-item"));
+
     var workWrapper = document.querySelector(".work__inner");
+    var tabsWrapper = document.querySelector(".tabs__wrapper");
+      var workButtons = Array.prototype.slice.call(tabsWrapper.querySelectorAll(".work__options-item"));
+    var workImgs = document.querySelectorAll(".tabs__img");
+    console.log(workImgs);
     var priceValue = document.querySelector('.tabs__inner-price-value');
-    function setDesktopBackground(workButton, index) {
+    function onStartCallbacks() {
+      priceValue.textContent = pricesArray[findIndex()];
+      for (var i = 0; i < workButtons.length; i++) {
+        var item = workButtons[i];
+        item.addEventListener("click", setItemImg(item, i));
+        item.classList.remove('work__options-item--active');
+        //workImgs[i].style = "display: none;";
+        workImgs[i].classList.add("visually-hidden");
+
+      }
+      //workImgs[findIndex()].style = "display: block;";
+      while (workImgs[findIndex()].classList.contains("visually-hidden")) {
+        workImgs[findIndex()].classList.remove("visually-hidden");
+      }
+      workButtons[findIndex()].classList.add('work__options-item--active');
+    }
+
+    function findIndex() {
+      var ind = 0;
+      for (var i = 0; i < workButtons.length; i++) {
+        var item = workButtons[i];
+        if (item.classList.contains('work__options-item--active')) {
+          ind = i;
+        }
+      }
+      return ind;
+    }
+
+    function setItemImg(workButton, index) {
       return function (evt) {
         evt.preventDefault();
-        workButton.classList.remove('work__options-item--active');
-        workButton.removeEventListener('click', setLaptopBackground);
-        workButton.removeEventListener('click', setTabletBackground);
-        workButton.removeEventListener('click', setMobileBackground);
-        workButtons.forEach(item => {
-          item.classList.remove('work__options-item--active');
-        });
-        workWrapper.style.background = 'url(' + imagesArray[index] + ') 845px 310px no-repeat,' + '  url("../images/work-back-desk.svg") 0px -72px no-repeat';
+        for (var i = 0; i < workButtons.length; i++) {
+          var item = workButtons[i];
+            item.classList.remove('work__options-item--active');
+            //workImgs[i].style = "display: none;";
+            workImgs[i].classList.add("visually-hidden");
+        }
+        //workImgs[findIndex()].style = "display: block;";
+        while (workImgs[index].classList.contains("visually-hidden")) {
+          workImgs[index].classList.remove("visually-hidden");
+        }
         workButton.classList.add('work__options-item--active');
         priceValue.textContent = pricesArray[index];
       };
     }
-    function setLaptopBackground(workButton, index) {
-      return function (evt) {
-        evt.preventDefault();
-        workButton.removeEventListener('click', setDesktopBackground);
-        workButton.removeEventListener('click', setTabletBackground);
-        workButton.removeEventListener('click', setMobileBackground);
-        workButtons.forEach(item => {
-          item.classList.remove('work__options-item--active');
-        });
-        var position = '595px 310px';
-        if (index === 2) {
-          position = '650px 370px';
-        }
-        workWrapper.style.background = 'url(' + imagesArray[index] + ')' + position + ' no-repeat,' + ' url("../images/border-mini.svg") 180px 250px no-repeat, url("../images/work-back-laptop.svg") -320px -75px no-repeat';
-          workButton.classList.add('work__options-item--active');
-          priceValue.textContent = pricesArray[index];
-          };
-
-        }
-    function setTabletBackground(workButton, index) {
-      return function (evt) {
-        evt.preventDefault();
-        workButton.removeEventListener('click', setDesktopBackground);
-        workButton.removeEventListener('click', setLaptopBackground);
-        workButton.removeEventListener('click', setMobileBackground);
-        workButtons.forEach(item => {
-          item.classList.remove('work__options-item--active');
-        });
-        var position = '170px 480px';
-        if (index === 2) {
-          position = '240px 480px';
-        }
-        if (index === 1) {
-          position = '240px 480px';
-        }
-            workWrapper.style.background = 'url(' + imagesTabletArray[index] + ') no-repeat ' + position +',' + ' url("../images/klass_avto_tabletka.svg") 0px -137px no-repeat';
-            workButton.classList.add('work__options-item--active');
-            priceValue.textContent = pricesArray[index];
-          };
-        }
-    function setMobileBackground(workButton, index) {
-      return function (evt) {
-        evt.preventDefault();
-        workButton.removeEventListener('click', setDesktopBackground);
-        workButton.removeEventListener('click', setTabletBackground);
-        workButton.removeEventListener('click', setLaptopBackground);
-        workButtons.forEach(item => {
-          item.classList.remove('work__options-item--active');
-        });
-            workWrapper.style.background = 'url("../images/work-back-mobile.svg") 0px 10px no-repeat, url(' + imagesMobileArray[index] + ') 0px 210px no-repeat';
-            workButton.classList.add('work__options-item--active');
-            priceValue.textContent = pricesArray[index];
-          };
-        }
-    function onStartCallbacks() {
-      priceValue.textContent = pricesArray[findIndex()];
-      if (document.body.clientWidth >= 1920) {
-        workWrapper.style.background = 'url(' + imagesArray[findIndex()] + ') 845px 310px no-repeat,' + '  url("../images/work-back-desk.svg") 0px -72px no-repeat';
-        workButtons.forEach((item, i) => {
-          item.addEventListener("click", setDesktopBackground(item, i));
-        });
-      }
-      else if (document.body.clientWidth < 1920 && document.body.clientWidth >= 1200) {
-        var position = '595px 310px';
-        if (findIndex() === 2) {
-          position = '650px 370px';
-        }
-        workWrapper.style.background = 'url(' + imagesArray[findIndex()] + ')' + position + ' no-repeat,' + ' url("../images/border-mini.svg") 180px 250px no-repeat, url("../images/work-back-laptop.svg") -320px -75px no-repeat';
-        workButtons.forEach((item, i) => {
-          item.addEventListener("click", setLaptopBackground(item, i));
-        });
-      }
-      else if (document.body.clientWidth < 1200 && document.body.clientWidth >= 768) {
-        var position = '170px 480px';
-        if (findIndex() === 2) {
-          position = '240px 480px';
-        }
-        if (findIndex() === 1) {
-          position = '240px 480px';
-        }
-        workWrapper.style.background = 'url(' + imagesTabletArray[findIndex()] + ') no-repeat ' + position +',' + ' url("../images/klass_avto_tabletka.svg") 0px -137px no-repeat';
-        workButtons.forEach((item, i) => {
-          item.addEventListener("click", setTabletBackground(item, i));
-        });
-      }
-      else {
-        workWrapper.style.background = 'url("../images/work-back-mobile.svg") 0px 10px no-repeat, url(' + imagesMobileArray[findIndex()] + ') 0px 210px no-repeat';
-        workButtons.forEach((item, i) => {
-          item.addEventListener("click", setMobileBackground(item, i));
-        });
-      }
-    }
-    window.onresize = function () {
-      onStartCallbacks();
-    };
-
-    function findIndex() {
-      var ind = 0;
-      workButtons.forEach((item, i) => {
-        if (item.classList.contains('work__options-item--active')) {
-          ind = i;
-        }
-      });
-      return ind;
-    }
-
     onStartCallbacks();
+
 })();
